8997b38f3a6036530261c2b99bbb497704d582c7
Alex Castro <acastro24@fordham.edu>
2022-02-08 11:49:04 -0500

    Initial working files Modified app.js and added TastList component via file import.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { Component } from "react";
+import { TaskList } from "./TaskList";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tasks: [],
+      complete: 0,
+      incomplete: 0
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Taskr</h1>
+        <h2>A basic todo list React app</h2>
+        <input name="taskInput" />
+        <p>
+          {this.state.incomplete} tasks done of {this.state.complete}
+        </p>
+        <TaskList />
+      </div>
+    );
+  }
 }
---
+++ src/TaskList.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export class TaskList extends Component {
+  render() {
+    return (
+      <ul className="navbar">
+        <li>Test</li>
+      </ul>
+    );
+  }
+}
